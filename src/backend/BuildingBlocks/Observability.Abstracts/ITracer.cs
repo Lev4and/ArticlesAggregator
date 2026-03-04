f7a8ba433da206c772ad315ade0ba72005d8f4c2
Lev4and <54065747+lev4and@users.noreply.github.com>
2026-03-04 21:06:40 +0500

    chore: Docker Swarm logging

---
+++ src/backend/BuildingBlocks/Observability.Abstracts/ITracer.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace Observability.Abstracts;
+
+public interface ITracer
+{
+    Activity StartOperation(string name, Dictionary<string, object?>? tags = null, 
+        ActivityKind kind = ActivityKind.Internal);
+}
---
+++ src/backend/BuildingBlocks/Observability.OpenTelemetry/Tracer.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Observability.Abstracts;
+
+namespace Observability.OpenTelemetry;
+
+public class Tracer : ITracer
+{
+    public Activity StartOperation(string name, Dictionary<string, object?>? tags = null, 
+        ActivityKind kind = ActivityKind.Internal)
+    {
+        throw new NotImplementedException();
+    }
+}
